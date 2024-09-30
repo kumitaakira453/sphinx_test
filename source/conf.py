a8c8d18dde34598f772ff115d55f5aec6fa50dc5
Kumita Akira <akira_kumita@demia.co.jp>
2024-09-30 16:42:48 +0900

    first_commit

---
+++ source/conf.py
@@ -0,0 +1,29 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# For the full list of built-in configuration values, see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Project information -----------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
+
+project = "sphinx-test"
+copyright = "2024, kumitaakira"
+author = "kumitaakira"
+
+# -- General configuration ---------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
+
+extensions = []
+
+templates_path = ["_templates"]
+exclude_patterns = []
+
+language = "ja"
+
+# -- Options for HTML output -------------------------------------------------
+# https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
+
+html_static_path = ["_static"]
+
+
+html_theme = "sphinx_rtd_theme"
